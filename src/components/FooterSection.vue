8873e64945bdb062dabfd336d1a03fda9f65b075
dieemedina <dmedina@techskies11.com>
2025-07-25 10:29:24 -0300

    Dark mode and translate

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="py-8 bg-black text-center text-gray-600 dark:text-gray-300">
+  <footer class="py-8 bg-white dark:bg-black text-center text-gray-600 dark:text-gray-300">
     <div class="w-full p-6 md:px-10 flex justify-between items-center flex-col md:flex-row gap-2 md:items-end">
       <div class="flex flex-col gap-2">
         <figure class="flex items-center gap-2">
@@ -11,7 +11,7 @@
           Lewes, Delaware 19958
         </p>
       </div>
-      <p class="opacity-70">The next generation of AI chat assistants, designed to help you achieve more.</p>
+      <p class="opacity-70">{{ $t('footer_legend') }}</p>
     </div>
     <hr class="my-2 border-gray-200 dark:border-gray-700">
     <div class="w-full px-6 md:px-10 flex justify-between items-center py-2 flex-col md:flex-row gap-2">
